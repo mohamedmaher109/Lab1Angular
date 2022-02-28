aae6146520c3e0d398f8f943807359bd75a74e2a
mohamedmaher109 <mohamedmaherali145@gmail.com>
2022-03-01 01:40:12 +0200

    second lab

--- myFirstApp/src/app/Shared Classes and types/DiscountOffers .ts
+++ myFirstApp/src/app/Shared Classes and types/DiscountOffers .ts
@@ -1,1 +1,1 @@
-export enum DiscountOffers {NoDiscount="No Discount",Discount10 = "10%",Discount15 = "15%"}
+export enum DiscountOffers {NoDiscount="No Discount",Discount10 = "0.10",Discount15 = "0.15%"}
--- myFirstApp/src/app/products/products.component.ts
+++ myFirstApp/src/app/products/products.component.ts
@@ -24,11 +24,19 @@ export class ProductsComponent implements OnInit {
     this.Discount    = DiscountOffers.Discount10;
     this.StoreName   = "Men Clothes";
     this.StoreLogo   = "../../assets/logo.jpg";
-    this.ProductList = [{Id:1,Name:"black-jacket",Price:1000,Quantity:1,Image:"../../assets/8.jpg"}];
-    this.CategoryList = [{Id:1,Name:"Jackets"}];
+    this.ProductList = [{Id:1,Name:"black-jacket",Price:1000,Quantity:1,Image:"../../assets/8.jpg"}
+                      ,{Id:2,Name:"slim-fit pantalon",Price:500,Quantity:1,Image:"../../assets/1.jpg"}];
+
+    this.CategoryList = [{Id:1,Name:"Jackets"},{Id:2,Name:"Pantalons"},{Id:3,Name:"T-shirts"}];
     this.ClientName = "Mohamed Maher";
-    this.IsPurshased = true;
+    this.IsPurshased = false;
    }
+
+   toggleShow() {
+
+    this.IsPurshased = ! this.IsPurshased;
+    
+    }
   
 
   ngOnInit(): void {
